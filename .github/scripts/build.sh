4769aa96dbd7a110684f999b9ee0163a2b5214ed
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-04-02 01:15:14 +0330

    GitHub automation

---
+++ .github/scripts/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Get the tag from the argument
+CI_COMMIT_TAG=$1
+
+# Docker Hub details
+USERNAME="blackiq"
+REPO_NAME="nginx"
+IMAGE_NAME="${USERNAME}/${REPO_NAME}"
+
+echo "🚀 Building and pushing ${IMAGE_NAME}:${CI_COMMIT_TAG} and latest for multiple architectures"
+
+# Enable Buildx if not already enabled
+docker buildx create --use || true
+
+# Build and push the multi-architecture image
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  -t ${IMAGE_NAME}:${CI_COMMIT_TAG} \
+  -t ${IMAGE_NAME}:latest \
+  --push .
+
+echo "✅ Docker images pushed successfully!"
